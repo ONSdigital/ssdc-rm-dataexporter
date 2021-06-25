5e4c0da0aba312356afe87164b864393de36085b
Hugh Brace <hugh.ons@gmail.com>
2021-06-25 08:28:56 +0100

    Migrate dataexporter to ssdc

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# ssdc-rm-dataexporter
+# ssdc-rm-data-exporter
+The Data Exporter is a scheduled batch job that runs once a day via a crontab job, or for a given start & end datetime. The data exporter exports a full set of case, event and qid information from the case database. 
+The data is exported in JSON format, zipped up and stored in a GCS bucket where it will be collected and sent downstream by MiNiFi.
+
+## Running locally
+The container is intended to be run within Kubernetes either as a cronjob (which will be run for the previous day as default), or with a start/end datetime.
+To run with a start/end datetime, connect to the one-off dataexport pod with bash, then run the job as follows:
+```
+START_DATETIME=<YYYY-MM-DDTHH:MM:SS> END_DATETIME=<YYYY-MM-DDTHH:MM:SS> ./run.sh
+```
+
+For example:
+```
+START_DATETIME=2020-12-01T00:00:00 END_DATETIME=2020-12-03T23:59:59.999999 ./run.sh
+```
+
+### connect to an RM test kubernetes cluster
+To test changes locally *connect to an existing RM test cluster* configured to support the dataexporter.
+
+e.g.
+gcloud beta container clusters get-credentials rm-k8s-cluster --region europe-west2 --project <SOME TEST PROJECT>
+
+### obtain service account credentials for the dataexport service account
+Create service account credentials for the dataexport service account and store in...
+*~/.config/gcloud/service-account-key.json*
+
+
+### store database connection certificates
+Create new connection certificates and store in your *~/.postgresql/* directory
+Ensure you are whitelisted against the test environment database.
+
+### Run the test script
+Use *./runlocaltest.sh*
+
+This will set the docker-compose environment variables based on the configmaps and secrets contained within the rm cluster you are connected to. *you must be whitelisted against the database for this to work*
+
+
+
---
+++ docker/run.sh
@@ -0,0 +1,233 @@
+#!/bin/bash
+echo "running rm-dataexporter job $(date)"
+
+# exit on errors, including unset variables (nounset)
+set -o errexit  
+set -o pipefail
+# set -o nounset
+# uncomment for trace
+# set -o xtrace
+
+FILENAME_DATE=$(date -u -d@"$(( `date -u +%s`-86400))" "+%Y-%m-%d")
+
+if [ -z "$START_DATETIME" ]; then
+  START_DATETIME=$(date -d@"$(( `date -u +%s`-86400))" "+%Y-%m-%dT00:00:00")
+  echo "START_DATETIME: ${START_DATETIME} [default]"
+else
+  echo "START_DATETIME: ${START_DATETIME}"
+fi
+
+if [ -z "$END_DATETIME" ]; then
+  END_DATETIME=$(date -u "+%Y-%m-%dT00:00:00")
+  echo "END_DATETIME: ${END_DATETIME} [default]"
+else
+  echo "END_DATETIME: ${END_DATETIME}"
+
+  # file naming to indicate between dates
+  FILENAME_DATE="${START_DATETIME}_to_${END_DATETIME}"
+fi
+
+############################################################################
+# REPLACE COLONS IN THE FILENAME DATE
+# Some of the consuming systems can't handle colons in filenames so we need to sanitise the date strings
+############################################################################
+FILENAME_DATE=${FILENAME_DATE//:/-}
+
+############################################################################
+# PREPARE POSTGRES CERTIFICATES
+# convert java pk8 required key format to pem if we're running in kubernetes
+############################################################################
+if [[ -f /root/.postgresql/postgresql.pk8 ]]; then
+  echo "creating client-key.pem file from postgresql.pk8"
+  openssl pkcs8 -in /root/.postgresql/postgresql.pk8 -inform der -nocrypt -out /tmp/client-key.pem
+  chmod 600 /tmp/client-key.pem
+fi
+
+# run all data exports in persistent volume mounted directory
+cd $PVC_MOUNT_PATH
+
+######################################################################
+# EXPORT UAC_QID_LINK TABLE AND UPLOAD FILE AND MANIFEST TO GCS BUCKET
+######################################################################
+echo "exporting uac_qid_link table content (no UACs)"
+
+
+QID_FILE=qid_$FILENAME_DATE.json
+
+PGPASSWORD=$DB_PASSWORD psql "sslmode=verify-ca sslrootcert=/root/.postgresql/root.crt sslcert=/root/.postgresql/postgresql.crt sslkey=/tmp/client-key.pem hostaddr=$DB_HOST port=$DB_PORT user=$DB_USERNAME dbname=rm" \
+-c "\copy (SELECT row_to_json(t) FROM (SELECT id,qid,caze_case_id as case_id,active,blank_questionnaire,ccs_case,created_date_time,last_updated FROM casev2.uac_qid_link where last_updated >= '$START_DATETIME' and last_updated < '$END_DATETIME') t) To '$QID_FILE';"
+
+
+if [ -n "$DATAEXPORT_MI_BUCKET_NAME" ]
+then
+    echo "adding uac_qid_link file $QID_FILE to bucket $DATAEXPORT_MI_BUCKET_NAME"
+    gsutil -q cp "$QID_FILE" gs://"$DATAEXPORT_MI_BUCKET_NAME"
+fi
+
+
+if [ -n "$DATAEXPORT_BUCKET_NAME" ]
+then
+  echo "zipping uac_qid_link file"
+
+  filename=SSDCResponseManagement_qid_$FILENAME_DATE.gz
+  gzip -c "$QID_FILE" > "$filename"
+
+  echo "adding $filename to bucket $DATAEXPORT_BUCKET_NAME"
+  gsutil -q cp "$filename" gs://"$DATAEXPORT_BUCKET_NAME"
+
+  echo "write manifest $filename.manifest"
+
+  cat > "$filename".manifest <<-EOF
+{
+  "schemaVersion": 1,
+  "files": [
+    {
+      "sizeBytes": $(stat -c%s $filename),
+      "md5sum": "$(openssl md5 "$filename" | awk '{ print $2 }')",
+      "relativePath": "./",
+      "name": "$filename"
+    }
+  ],
+  "sourceName": "RM",
+  "manifestCreated": "$(date +'%Y-%m-%dT%H:%M:%S').0000000Z",
+  "description": "RM uac_qid_link table export",
+  "dataset": "RM_cases",
+  "version": 1
+}
+EOF
+
+
+  echo "adding $filename.manifest to bucket $DATAEXPORT_BUCKET_NAME"
+  gsutil -q cp "$filename".manifest gs://"$DATAEXPORT_BUCKET_NAME"
+
+  # cleanup files
+  rm "$filename".manifest
+  rm $filename
+fi
+
+# cleanup file
+rm $QID_FILE
+
+######################################################################
+# EXPORT CASES TABLE AND UPLOAD FILE AND MANIFEST TO GCS BUCKET
+######################################################################
+echo "exporting cases table content"
+
+
+CASES_FILE=cases_$FILENAME_DATE.json
+
+PGPASSWORD=$DB_PASSWORD psql "sslmode=verify-ca sslrootcert=/root/.postgresql/root.crt sslcert=/root/.postgresql/postgresql.crt sslkey=/tmp/client-key.pem hostaddr=$DB_HOST port=$DB_PORT user=$DB_USERNAME dbname=rm" \
+-c "\copy (SELECT row_to_json(t) FROM (SELECT * FROM casev2.cases where last_updated >= '$START_DATETIME' and last_updated < '$END_DATETIME') t) To '$CASES_FILE';"
+
+
+if [ -n "$DATAEXPORT_MI_BUCKET_NAME" ]
+then
+    echo "adding cases file $CASES_FILE to bucket $DATAEXPORT_MI_BUCKET_NAME"
+    gsutil -q cp "$CASES_FILE" gs://"$DATAEXPORT_MI_BUCKET_NAME"
+fi
+
+
+if [ -n "$DATAEXPORT_BUCKET_NAME" ]
+then
+  echo "zipping cases file"
+
+  filename=SSDCResponseManagement_case_$FILENAME_DATE.gz
+  gzip -c "$CASES_FILE" > "$filename"
+
+  echo "adding $filename to bucket $DATAEXPORT_BUCKET_NAME"
+  gsutil -q cp "$filename" gs://"$DATAEXPORT_BUCKET_NAME"
+
+  echo "write manifest $filename.manifest"
+
+  cat > "$filename".manifest <<-EOF
+{
+  "schemaVersion": 1,
+  "files": [
+    {
+      "sizeBytes": $(stat -c%s $filename),
+      "md5sum": "$(openssl md5 "$filename" | awk '{ print $2 }')",
+      "relativePath": "./",
+      "name": "$filename"
+    }
+  ],
+  "sourceName": "RM",
+  "manifestCreated": "$(date +'%Y-%m-%dT%H:%M:%S').0000000Z",
+  "description": "RM cases table export",
+  "dataset": "RM_cases",
+  "version": 1
+}
+EOF
+
+  echo "adding $filename.manifest to bucket $DATAEXPORT_BUCKET_NAME"
+  gsutil -q cp "$filename".manifest gs://"$DATAEXPORT_BUCKET_NAME"
+
+  # cleanup files
+  rm $filename
+  rm "$filename".manifest
+fi
+
+# cleanup file
+rm $CASES_FILE
+
+######################################################################
+# EXPORT EVENT TABLE AND UPLOAD FILE AND MANIFEST TO GCS BUCKET
+######################################################################
+echo "exporting event table content"
+
+
+EVENTS_FILE=events_$FILENAME_DATE.json
+
+PGPASSWORD=$DB_PASSWORD psql "sslmode=verify-ca sslrootcert=/root/.postgresql/root.crt sslcert=/root/.postgresql/postgresql.crt sslkey=/tmp/client-key.pem hostaddr=$DB_HOST port=$DB_PORT user=$DB_USERNAME dbname=rm" \
+-c "\copy (SELECT row_to_json(t) FROM (SELECT * FROM casev2.event where event_type!='CASE_CREATED' and event_type!='UAC_UPDATED' and event_type!='SAMPLE_LOADED' and event_type!='RM_UAC_CREATED' and rm_event_processed >= '$START_DATETIME' and rm_event_processed < '$END_DATETIME') t) To '$EVENTS_FILE';"
+
+
+if [ -n "$DATAEXPORT_MI_BUCKET_NAME" ]
+then
+    echo "adding event file $EVENTS_FILE to bucket $DATAEXPORT_MI_BUCKET_NAME"
+    gsutil -q cp "$EVENTS_FILE" gs://"$DATAEXPORT_MI_BUCKET_NAME"
+fi
+
+
+if [ -n "$DATAEXPORT_BUCKET_NAME" ]
+then
+  echo "zipping event file"
+
+  filename=SSDCResponseManagement_events_$FILENAME_DATE.gz
+  gzip -c "$EVENTS_FILE" > "$filename"
+
+  echo "adding $filename to bucket $DATAEXPORT_BUCKET_NAME"
+  gsutil -q cp "$filename" gs://"$DATAEXPORT_BUCKET_NAME"
+
+  echo "write manifest $filename.manifest"
+
+  cat > "$filename".manifest <<-EOF
+{
+  "schemaVersion": 1,
+  "files": [
+    {
+      "sizeBytes": $(stat -c%s $filename),
+      "md5sum": "$(openssl md5 "$filename" | awk '{ print $2 }')",
+      "relativePath": "./",
+      "name": "$filename"
+    }
+  ],
+  "sourceName": "RM",
+  "manifestCreated": "$(date +'%Y-%m-%dT%H:%M:%S').0000000Z",
+  "description": "RM event table export",
+  "dataset": "RM_cases",
+  "version": 1
+}
+EOF
+
+  echo "adding $filename.manifest to bucket $DATAEXPORT_BUCKET_NAME"
+  gsutil -q cp "$filename".manifest gs://"$DATAEXPORT_BUCKET_NAME"
+
+  # cleanup files
+  rm $filename
+  rm "$filename".manifest
+fi
+
+echo "file export complete"
+
+# cleanup file
+rm $EVENTS_FILE
